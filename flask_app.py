9035ccf6f94d1e61f7f20dddf0a2cb72d36ded53
Peter van Es <peter.van.es@solconmail.nl>
2023-05-12 20:59:26 +0200

    Opdrachten van les 11

--- flask_app.py
+++ flask_app.py
@@ -1,5 +1,9 @@
 from flask import Flask
 app=Flask(__name__)
 
+@app.route('/')
+def home():
+    return "<p><em><b>Les 11 vraag 4: Wij maken de hoofdtak 'main' aan en we schakelen daar naartoe over.</b></em></p>"
+
 if __name__ == '__main__':
     app.run(port=5000,debug=True)
